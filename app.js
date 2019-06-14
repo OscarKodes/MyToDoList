6c09d197c183a8400f06407cd57d0a24d8fac0bc
Oscar Ko <oscarkoemail@gmail.com>
2019-06-14 17:16:31 -0400

    Add todo list functionality, add items, update list

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 //jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
 
+const items = ["Eat Food", "Cook Food", "Buy Food"];
+
+app.set('view engine', 'ejs');
+
+// Tell express to serve up the public folder as a static resource
+app.use(express.static(__dirname + '/public'));
+// Tells express to use bodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.send("Hello");
+
+  let options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric"
+  }
+
+  let today = new Date().toLocaleDateString("en-US", options);
+
+  res.render("home", {day: today, newListItems: items});
+});
+
+app.post("/", function(req, res) {
+  let newItem = req.body.newItem;
+
+  items.push(newItem);
+  res.redirect("/");
 });
 
 
